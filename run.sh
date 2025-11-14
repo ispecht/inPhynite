a52edb60cfc6f877c5d145e76482608e4963fd2b
Ivan Specht <ispecht@college.harvard.edu>
2025-11-14 14:14:31 -0800

    run script cleanup

--- run.sh
+++ run.sh
@@ -1,33 +1,34 @@
-# Fixed nt at 1, 2, 4, 8
-#./inPhynite --nt_init 4.0 --nt_log_slope 0.0 --n 32 --rooted 0 --n_global_iters 10000 --n_tree_iters 10000 --sample_every 10 --infer_nt 0 --out_dir /Users/ispecht/Desktop/PhyloSMC-analyses/runs/PhyloSMC/fixed_N_eff/run_1
+# Fixed nt
+./inPhynite --nt_init 4.0 --nt_log_slope 0.0 --n 32 --rooted 0 --n_global_iters 10000 --n_tree_iters 10000 --sample_every 10 --infer_nt 0 --out_dir /Users/ispecht/Desktop/inPhynite-analyses/runs/inPhynite/fixed_N_eff/run_1
 
-#./inPhynite --nt_init 16.0 --nt_log_slope 0.0 --n 32 --rooted 0 --n_global_iters 10000 --n_tree_iters 10000 --sample_every 10 --infer_nt 0 --out_dir /Users/ispecht/Desktop/PhyloSMC-analyses/runs/PhyloSMC/fixed_N_eff/run_2
+./inPhynite --nt_init 16.0 --nt_log_slope 0.0 --n 32 --rooted 0 --n_global_iters 10000 --n_tree_iters 10000 --sample_every 10 --infer_nt 0 --out_dir /Users/ispecht/Desktop/inPhynite-analyses/runs/inPhynite/fixed_N_eff/run_2
 
-#./inPhynite --nt_init 64.0 --nt_log_slope 0.0 --n 32 --rooted 0 --n_global_iters 10000 --n_tree_iters 10000 --sample_every 10 --infer_nt 0 --out_dir /Users/ispecht/Desktop/PhyloSMC-analyses/runs/PhyloSMC/fixed_N_eff/run_3
+./inPhynite --nt_init 64.0 --nt_log_slope 0.0 --n 32 --rooted 0 --n_global_iters 10000 --n_tree_iters 10000 --sample_every 10 --infer_nt 0 --out_dir /Users/ispecht/Desktop/inPhynite-analyses/runs/inPhynite/fixed_N_eff/run_3
 
-#./inPhynite --nt_init 4.0 --nt_log_slope 0.0 --n 128 --rooted 0 --n_global_iters 10000 --n_tree_iters 10000 --sample_every 10 --infer_nt 0 --out_dir /Users/ispecht/Desktop/PhyloSMC-analyses/runs/PhyloSMC/fixed_N_eff/run_4
+./inPhynite --nt_init 4.0 --nt_log_slope 0.0 --n 128 --rooted 0 --n_global_iters 10000 --n_tree_iters 10000 --sample_every 10 --infer_nt 0 --out_dir /Users/ispecht/Desktop/inPhynite-analyses/runs/inPhynite/fixed_N_eff/run_4
 
-#./inPhynite --nt_init 16.0 --nt_log_slope 0.0 --n 128 --rooted 0 --n_global_iters 10000 --n_tree_iters 10000 --sample_every 10 --infer_nt 0 --out_dir /Users/ispecht/Desktop/PhyloSMC-analyses/runs/PhyloSMC/fixed_N_eff/run_5
+./inPhynite --nt_init 16.0 --nt_log_slope 0.0 --n 128 --rooted 0 --n_global_iters 10000 --n_tree_iters 10000 --sample_every 10 --infer_nt 0 --out_dir /Users/ispecht/Desktop/inPhynite-analyses/runs/inPhynite/fixed_N_eff/run_5
 
-#./inPhynite --nt_init 64.0 --nt_log_slope 0.0 --n 128 --rooted 0 --n_global_iters 10000 --n_tree_iters 10000 --sample_every 10 --infer_nt 0 --out_dir /Users/ispecht/Desktop/PhyloSMC-analyses/runs/PhyloSMC/fixed_N_eff/run_6
+./inPhynite --nt_init 64.0 --nt_log_slope 0.0 --n 128 --rooted 0 --n_global_iters 10000 --n_tree_iters 10000 --sample_every 10 --infer_nt 0 --out_dir /Users/ispecht/Desktop/inPhynite-analyses/runs/inPhynite/fixed_N_eff/run_6
 
-#./inPhynite --nt_init 4.0 --nt_log_slope 0.0 --n 512 --rooted 0 --n_global_iters 10000 --n_tree_iters 10000 --sample_every 10 --infer_nt 0 --out_dir /Users/ispecht/Desktop/PhyloSMC-analyses/runs/PhyloSMC/fixed_N_eff/run_7
+./inPhynite --nt_init 4.0 --nt_log_slope 0.0 --n 512 --rooted 0 --n_global_iters 10000 --n_tree_iters 10000 --sample_every 10 --infer_nt 0 --out_dir /Users/ispecht/Desktop/inPhynite-analyses/runs/inPhynite/fixed_N_eff/run_7
 
-#./inPhynite --nt_init 16.0 --nt_log_slope 0.0 --n 512 --rooted 0 --n_global_iters 10000 --n_tree_iters 10000 --sample_every 10 --infer_nt 0 --out_dir /Users/ispecht/Desktop/PhyloSMC-analyses/runs/PhyloSMC/fixed_N_eff/run_8
+./inPhynite --nt_init 16.0 --nt_log_slope 0.0 --n 512 --rooted 0 --n_global_iters 10000 --n_tree_iters 10000 --sample_every 10 --infer_nt 0 --out_dir /Users/ispecht/Desktop/inPhynite-analyses/runs/inPhynite/fixed_N_eff/run_8
 
-#./inPhynite --nt_init 64.0 --nt_log_slope 0.0 --n 512 --rooted 0 --n_global_iters 10000 --n_tree_iters 10000 --sample_every 10 --infer_nt 0 --out_dir /Users/ispecht/Desktop/PhyloSMC-analyses/runs/PhyloSMC/fixed_N_eff/run_9
+./inPhynite --nt_init 64.0 --nt_log_slope 0.0 --n 512 --rooted 0 --n_global_iters 10000 --n_tree_iters 10000 --sample_every 10 --infer_nt 0 --out_dir /Users/ispecht/Desktop/inPhynite-analyses/runs/inPhynite/fixed_N_eff/run_9
 
-# Inferred nt, start 128, log slope -1
-#./inPhynite --nt_init 128.0 --nt_log_slope -1.0 --n 40 --rooted 0 --n_global_iters 100000 --n_tree_iters 10000 --sample_every 100 --infer_nt 1 --out_dir /Users/ispecht/Desktop/PhyloSMC-analyses/runs/PhyloSMC/inferred_N_eff/drift_-1
+# Inferred nt
+./inPhynite --nt_init 128.0 --nt_log_slope -1.0 --n 40 --rooted 0 --n_global_iters 100000 --n_tree_iters 10000 --sample_every 100 --infer_nt 1 --out_dir /Users/ispecht/Desktop/inPhynite-analyses/runs/inPhynite/inferred_N_eff/drift_-1
 
-# Inferred nt, constant 32
-#./inPhynite --nt_init 32.0 --nt_log_slope 0.0 --n 40 --rooted 0 --n_global_iters 100000 --n_tree_iters 10000 --sample_every 100 --infer_nt 1 --out_dir /Users/ispecht/Desktop/PhyloSMC-analyses/runs/PhyloSMC/inferred_N_eff/drift_0
+./inPhynite --nt_init 32.0 --nt_log_slope 0.0 --n 40 --rooted 0 --n_global_iters 100000 --n_tree_iters 10000 --sample_every 100 --infer_nt 1 --out_dir /Users/ispecht/Desktop/inPhynite-analyses/runs/inPhynite/inferred_N_eff/drift_0
 
-# start 1.5, log slope 1
-#./inPhynite --nt_init 1.5 --nt_log_slope 1.0 --n 40 --rooted 0 --n_global_iters 100000 --n_tree_iters 10000 --sample_every 100 --infer_nt 1 --out_dir /Users/ispecht/Desktop/PhyloSMC-analyses/runs/PhyloSMC/inferred_N_eff/drift_1
+./inPhynite --nt_init 1.5 --nt_log_slope 1.0 --n 40 --rooted 0 --n_global_iters 100000 --n_tree_iters 10000 --sample_every 100 --infer_nt 1 --out_dir /Users/ispecht/Desktop/inPhynite-analyses/runs/inPhynite/inferred_N_eff/drift_1
 
 # Real data
-./inPhynite --rooted 0 --n_global_iters 100000 --n_tree_iters 10000 --sample_every 100 --infer_nt 1 --in_file /Users/ispecht/Desktop/PhyloSMC-analyses/data/YRI.fasta --out_dir /Users/ispecht/Desktop/PhyloSMC-analyses/runs/PhyloSMC/real_data/YRI
-./inPhynite --rooted 0 --n_global_iters 100000 --n_tree_iters 10000 --sample_every 100 --infer_nt 1 --in_file /Users/ispecht/Desktop/PhyloSMC-analyses/data/FIN.fasta --out_dir /Users/ispecht/Desktop/PhyloSMC-analyses/runs/PhyloSMC/real_data/FIN
-./inPhynite --rooted 0 --n_global_iters 100000 --n_tree_iters 10000 --sample_every 100 --infer_nt 1 --in_file /Users/ispecht/Desktop/PhyloSMC-analyses/data/PEL.fasta --out_dir /Users/ispecht/Desktop/PhyloSMC-analyses/runs/PhyloSMC/real_data/PEL
-./inPhynite --rooted 0 --n_global_iters 100000 --n_tree_iters 10000 --sample_every 100 --infer_nt 1 --in_file /Users/ispecht/Desktop/PhyloSMC-analyses/data/CEU.fasta --out_dir /Users/ispecht/Desktop/PhyloSMC-analyses/runs/PhyloSMC/real_data/CEU
+./inPhynite --rooted 0 --n_global_iters 100000 --n_tree_iters 10000 --sample_every 100 --infer_nt 1 --in_file /Users/ispecht/Desktop/inPhynite-analyses/data/CEU.fasta --out_dir /Users/ispecht/Desktop/inPhynite-analyses/runs/inPhynite/real_data/CEU
+
+./inPhynite --rooted 0 --n_global_iters 100000 --n_tree_iters 10000 --sample_every 100 --infer_nt 1 --in_file /Users/ispecht/Desktop/inPhynite-analyses/data/FIN.fasta --out_dir /Users/ispecht/Desktop/inPhynite-analyses/runs/inPhynite/real_data/FIN
+
+./inPhynite --rooted 0 --n_global_iters 100000 --n_tree_iters 10000 --sample_every 100 --infer_nt 1 --in_file /Users/ispecht/Desktop/inPhynite-analyses/data/PEL.fasta --out_dir /Users/ispecht/Desktop/inPhynite-analyses/runs/inPhynite/real_data/PEL
+
+./inPhynite --rooted 0 --n_global_iters 100000 --n_tree_iters 10000 --sample_every 100 --infer_nt 1 --in_file /Users/ispecht/Desktop/inPhynite-analyses/data/YRI.fasta --out_dir /Users/ispecht/Desktop/inPhynite-analyses/runs/inPhynite/real_data/YRI
